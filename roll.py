004fd3e08c1a85feb7413892fb26a829bd72f3b5
c00139891 <c00139891@gmail.com>
2016-06-30 00:12:56 +0100

    Update roll.py

--- roll.py
+++ roll.py
@@ -1,1 +1,20 @@
-print("What sided dice would you like to use?")
+import random
+import sys
+
+def roll_dice(dice_sides):
+    while True:
+        user_input = input("Would you like to role the dice? (yes/no) ")
+        if user_input in ['yes', 'Yes', 'y', 'no', 'No', 'n']:
+            break;
+        else:
+            print("This is not a valid option. Would you like to role the dice? (yes/no)")
+    if user_input in ['yes', 'Yes', 'y']:
+        dice_roll = random.randint(1,dice_sides)
+        return dice_roll
+    elif user_input in ['no', 'No', 'n']:
+        print("Ok no roll will take place")
+
+
+dice_sides = int(input("What side dice do you wish to use? "))
+result = roll_dice(dice_sides)
+print(result) 
